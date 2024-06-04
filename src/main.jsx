f178fdf20661fe4ef12ec56f4e16e39f18cf8790
abrilxcx <abrlbjrn@gmail.com>
2024-06-04 17:51:37 -0600

    Created HooksApp and implemented in main

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import './index.css';
+import { HooksApp } from './HooksApp';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <HooksApp />
   </React.StrictMode>,
 )
