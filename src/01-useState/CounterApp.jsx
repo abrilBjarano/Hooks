41e0b979981fbfbe2f87b0ad2140adc4f0cb2a00
abrilxcx <abrlbjrn@gmail.com>
2024-06-05 12:13:31 -0600

    Updated dev script from package.json

--- src/01-useState/CounterApp.jsx
+++ src/01-useState/CounterApp.jsx
@@ -11,13 +11,18 @@ export const CounterApp = () => {
 
    return (
       <>
-         <h1>Counter: { counter1 }</h1>
+         <h1>Counter1: { counter1 }</h1>
+         <h1>Counter2: { counter2 }</h1>
+         <h1>Counter3: { counter3 }</h1>
 
          <hr />
 
          <button 
             className="btn" 
-            onClick={ () => setCounter( counter1 + 1 ) }
+            onClick={ () => setCounter({
+               ...{ counter1, counter2, counter3 },
+               counter1: counter1 + 1
+            })}
          > +1 </button>
       </>
    )
