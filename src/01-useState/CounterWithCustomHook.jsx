ada46a932ced0ccce51c8e2b3e84191c4da393c9
abrilxcx <abrlbjrn@gmail.com>
2024-06-05 13:01:56 -0600

    Implemented useCounterr to CounterWithCustomHook

--- src/01-useState/CounterWithCustomHook.jsx
+++ src/01-useState/CounterWithCustomHook.jsx
@@ -1,7 +1,13 @@
+import { useCounter } from "../hooks/useCounter";
+
 export const CounterWithCustomHook = () => {
+
+   const { counter } = useCounter();
+
+
   return (
     <>
-      <h1>Counter with Hook: XXX</h1>
+      <h1>Counter with Hook: { counter } </h1>
       <hr />
 
       <button className="btn btn-primary">-1</button>
