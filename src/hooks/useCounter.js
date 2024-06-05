1213e4aff8d177d3053ad8dc6ea9bfb04227eb87
abrilxcx <abrlbjrn@gmail.com>
2024-06-05 12:58:53 -0600

    Changed useConter to export

--- src/hooks/useCounter.js
+++ src/hooks/useCounter.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-const useCounter = ( initialValue = 10 ) => {
+export const useCounter = ( initialValue = 10 ) => {
 
    const [counter, setCounter] = useState( initialValue );
 
