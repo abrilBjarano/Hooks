3bf188dba6eeeb3bc78ed0fd24a3cbc3b43a5186
abrilxcx <abrlbjrn@gmail.com>
2024-06-06 14:11:52 -0600

    Deactivatede StrictMode

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import './index.css';
 import { SimpleForm } from './02-useEffect/SimpleForm';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <SimpleForm />
-  </React.StrictMode>,
+  // </React.StrictMode>,
 )
