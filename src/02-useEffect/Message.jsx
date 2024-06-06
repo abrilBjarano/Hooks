fcf4f05b600a304381b8e69d32c42cf77327de55
abrilxcx <abrlbjrn@gmail.com>
2024-06-06 14:38:36 -0600

    Updated Message

--- src/02-useEffect/Message.jsx
+++ src/02-useEffect/Message.jsx
@@ -1,4 +1,17 @@
+import { useEffect } from "react"
+
 export const Message = () => {
+
+   useEffect(() => {
+      console.log('Message Mounted');
+      
+      return () => {
+         console.log('Message Unmounted');
+     }
+   }, [])
+   
+
+
   return (
     <>
       <h3>Usuario ya existe</h3>
