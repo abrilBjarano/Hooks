c7b26cf585d3e53bce87778a4ac3dec48c1ead5a
abrilxcx <abrlbjrn@gmail.com>
2024-06-07 11:30:13 -0600

    Created FormWithCustomHook.jsx and implemented into main

---
+++ src/02-useEffect/FormWithCustomHook.jsx
@@ -0,0 +1,66 @@
+import { useEffect, useState } from "react";
+import { Message } from "./Message";
+
+export const FormWithCustomHook = () => {
+
+   const [formState, setFormState] = useState({
+      username: 'abril',
+      email: 'abril@google.com'
+   })
+
+   const { username, email } = formState;
+
+
+   const onInputChange = ({ target }) => {
+      const { name, value } = target;
+      setFormState({
+         ...formState,
+         [name]: value
+      }, );
+   };
+
+
+   useEffect(() => {
+    //  console.log('useEffect called')
+   }, []);
+
+   useEffect(() => {
+    //  console.log('formState changed')
+   }, [ formState ]);
+
+   useEffect(() => {
+    //  console.log('formState email changed')
+   }, [ email ]);
+   
+   
+
+
+  return (
+    <>
+      <h1>Formulario con custom Hook</h1>
+      <hr />
+
+
+      <input 
+         type="text"
+         className="form-control"
+         placeholder="Username"
+         name="username"
+         value={ username }
+         onChange={ onInputChange }
+      />
+
+      <input 
+         type="email"
+         className="form-control mt-2 mb-2"
+         placeholder="eMail"
+         name="email"
+         value={ email }
+         onChange={ onInputChange }
+      />
+
+      { username === 'abril2' && <Message /> }
+    </>
+  )
+}
+// 
--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import './index.css';
-import { SimpleForm } from './02-useEffect/SimpleForm';
+import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-    <SimpleForm />
+    <FormWithCustomHook />
   // </React.StrictMode>,
 )
