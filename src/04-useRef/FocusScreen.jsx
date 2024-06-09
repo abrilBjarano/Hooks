3714da0e1c24e872c6cbff8922f80ff1f2849bf1
abrilxcx <abrlbjrn@gmail.com>
2024-06-09 16:07:54 -0600

    Created FocusScreen.jsx and implemented into main

---
+++ src/04-useRef/FocusScreen.jsx
@@ -0,0 +1,24 @@
+export const FocusScreen = () => {
+
+
+
+
+
+  return (
+    <>
+      <h1>Focus Screen</h1>
+      <hr />
+
+      <input 
+         type="text"
+         placeholder="Enter your name"
+         className="form-control"
+      />
+
+      <div 
+         className="btn btn-primary mt-2">
+         Set focus
+      </div>
+    </>
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import './index.css';
-import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks';
+import { FocusScreen } from './04-useRef/FocusScreen';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-    <MultipleCustomHooks />
+    <FocusScreen />
   // </React.StrictMode>,
 )
