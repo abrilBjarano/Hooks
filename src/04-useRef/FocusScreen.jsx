0d528da1b4ea8e3ddd3c5970b5facdd34e36f131
abrilxcx <abrlbjrn@gmail.com>
2024-06-09 16:49:20 -0600

    Added inputRef to FocusScreen

--- src/04-useRef/FocusScreen.jsx
+++ src/04-useRef/FocusScreen.jsx
@@ -1,8 +1,12 @@
+import { useRef } from "react";
+
 export const FocusScreen = () => {
 
+   const inputRef = useRef();
 
-
-
+   const onClick = () => {
+      inputRef.current.select();
+   };
 
   return (
     <>
@@ -10,14 +14,16 @@ export const FocusScreen = () => {
       <hr />
 
       <input 
+         ref={ inputRef }
          type="text"
          placeholder="Enter your name"
          className="form-control"
-      />
+         />
 
       <div 
-         className="btn btn-primary mt-2">
-         Set focus
+         className="btn btn-primary mt-2"
+         onClick={ onClick }
+      > Set focus
       </div>
     </>
   )
