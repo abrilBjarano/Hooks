5f7a8cd1b169270c9e2edda6f02cbc4240cc1010
abrilxcx <abrlbjrn@gmail.com>
2024-06-10 15:55:38 -0600

    Created LoadingQuote.jsx and Quote.jsx
    And implemented to Layout

--- src/05-useLayoutEffect/Layout.jsx
+++ src/05-useLayoutEffect/Layout.jsx
@@ -1,49 +1,33 @@
-import { useFetch } from '../hooks';
-import { PokemonCard } from '../03-examples/PokemonCard';
+import { useFetch } from '../hooks/useFetch';
 import { useCounter } from '../hooks/useCounter';
-import { LoadingMessage } from '../03-examples/LoadingMessage';
+import { LoadingQuote } from './LoadingQuote';
+import { Quote } from './Quote';
+
 
 export const Layout = () => {
 
-  const { counter, increment, decrement } = useCounter(1);
+    const { counter, increment } = useCounter(1);
+    const { data, isLoading, hasError } = useFetch(`https://api.breakingbadquotes.xyz/v1/quotes/${ counter }`);
+    const { author, quote } = !!data && data[0];
+    
+    return (
+        <>
+            <h1>BreakingBad Quotes</h1>
+            <hr />
 
-   const { data, hasError, isLoading } = useFetch(`https://pokeapi.co/api/v2/pokemon/${ counter }`);
-
-  return (
-     <>
-      <h1>Pokemon's information</h1>
-      <hr />
-
-      { 
-        isLoading 
-          ? <LoadingMessage /> 
-          : <PokemonCard 
-            id={ data.id } 
-            name={ data.name }  
-            sprites={[ 
-              data.sprites.front_default,
-              data.sprites.front_shiny,
-              data.sprites.back_default,
-              data.sprites.back_shiny
-            ]}
-            abilities={
-              data.abilities[0].ability.name
+            {
+                isLoading
+                 ? <LoadingQuote />
+                 : <Quote author={ author } quote={ quote } />
             }
-            /> 
-      }
+                      
+            <button 
+                className="btn btn-primary"
+                disabled={ isLoading }
+                onClick={ () => increment() }>
+                Next quote 
+            </button>
 
-      <button 
-        className='btn btn-primary mt-2'
-        onClick={ () => counter > 1 ? decrement() : null }
-      > Anterior
-      </button>
-
-      <button 
-        className='btn btn-primary mt-2'
-        onClick={ () => increment() }
-      > Siguiente
-      </button>
-
-    </>
-  )
+        </>
+    )
 }
