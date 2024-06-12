5629c39d550e09e74bf9ee7e4e2c7482f9359f84
abrilxcx <abrlbjrn@gmail.com>
2024-06-12 10:15:41 -0600

    Added Hijo.jsx and Padre.jsx
    Ready to perform task

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import './index.css';
-import { CallbackHook } from './06-memos/CallbackHook';
+import { Padre } from './07-tarea-memo/Padre';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-    <CallbackHook />
+    <Padre />
   // </React.StrictMode>,
 )
