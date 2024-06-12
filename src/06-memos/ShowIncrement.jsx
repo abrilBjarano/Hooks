ab6d10643124635b62a83e82de709a7be80d8b70
abrilxcx <abrlbjrn@gmail.com>
2024-06-12 10:10:36 -0600

    Updated onClick button format

--- src/06-memos/ShowIncrement.jsx
+++ src/06-memos/ShowIncrement.jsx
@@ -7,9 +7,7 @@ export const ShowIncrement = React.memo(({ increment }) => {
   return (
     <button
       className="btn btn-primary"
-      onClick={() => {
-         increment( 5 );
-      }}
+      onClick={() => { increment( 5 ) }}
     >
       Incrementar
     </button>
