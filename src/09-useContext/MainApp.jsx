2aec776b73085ae82046fb5c48e13e69759a0a86
abrilxcx <abrlbjrn@gmail.com>
2024-06-23 15:49:04 -0600

    Configured Routes in MainAppp

--- src/09-useContext/MainApp.jsx
+++ src/09-useContext/MainApp.jsx
@@ -1,8 +1,22 @@
+import { Route, Routes } from "react-router-dom";
+
+import { HomePage } from "./HomePage";
+import { AboutPage } from "./AboutPage";
+import { LoginPage } from "./LoginPage";
+
 export const MainApp = () => {
    return (
       <>
          <h1>MainApp</h1>
          <hr />
+
+         <Routes>
+            <Route path="/" element={ <HomePage/> }/>
+            <Route path="about" element={ <AboutPage/> }/>
+            <Route path="login" element={ <LoginPage/> }/>
+
+            <Route path="/*" element={ <LoginPage /> } />
+         </Routes>
       </>
    )
 }
