9f09a90b62d8b07bbbf14f623cdcc4b598058f48
abrilxcx <abrlbjrn@gmail.com>
2024-06-23 13:52:17 -0600

    Created pages and imported MainApp to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import './index.css';
-import { TodoApp } from './08-useReducer/TodoApp';
+import { MainApp } from './09-useContext/MainApp';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-    <TodoApp />
+    <MainApp />
   // </React.StrictMode>,
 )
