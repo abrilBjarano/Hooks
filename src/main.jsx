20aa233ec3996013a7110c1b40f38b1b7343a78f
abrilxcx <abrlbjrn@gmail.com>
2024-06-23 14:24:07 -0600

    Install and configure React Router

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom';
+
 import './index.css';
 import { MainApp } from './09-useContext/MainApp';
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <MainApp />,
+  }
+]);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-    <MainApp />
+    <RouterProvider router={ router } />
   // </React.StrictMode>,
 )
