e28972208d04f3b65049ce68d25219825835f974
abrilxcx <abrlbjrn@gmail.com>
2024-06-24 12:03:52 -0600

    Consumed context in LoginPage

--- src/09-useContext/LoginPage.jsx
+++ src/09-useContext/LoginPage.jsx
@@ -1,4 +1,12 @@
+import { useContext } from "react";
+import { UserContext } from "./context/UserContext";
+
 export const LoginPage = () => {
+
+   const { hola, user } = useContext( UserContext );
+   console.log(user);
+
+
    return (
       <>
          <h1>LoginPage</h1>
